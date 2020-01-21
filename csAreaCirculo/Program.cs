3de7dc61140c3dc49c38792bd8214e545dba0336
Sergio Javier Vergara Ganado <sevega69@msn.com>
2020-01-20 21:00:54 -0600

    Conoce como agregar contenido al método WriteLine

---
+++ csAreaCirculo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace csAreaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración de variables
+            double area;
+            int radio = 5;
+            const double pi = 3.1416;
+            // Proceso
+            area = pi * (radio * 2);
+            // Salida de datos
+            Console.WriteLine($"El área del circulo es de {area}" +
+                $" tomando como radio un valor de {radio}");
+
+        }
+    }
+}
